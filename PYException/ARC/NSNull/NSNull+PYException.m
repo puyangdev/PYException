193e3ba9d0367e6769a28d56815a33140d37b085
Bc.wh1te.Lei <baihaotian@51xianqu.net>
2018-10-23 15:27:38 +0800

    finish expend NSNull

--- PYException/ARC/NSNull/NSNull+PYException.m
+++ PYException/ARC/NSNull/NSNull+PYException.m
@@ -17,14 +17,40 @@
     dispatch_once(&onceToken, ^{
         @autoreleasepool {
             if (!DEBUG_FLAG) {
-                 [objc_getClass("NSNull") py_swizzleMethod:@selector(length) swizzledSelector:@selector(py_replace_length)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(length) swizzledSelector:@selector(py_replace_length)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(integerValue) swizzledSelector:@selector(py_integerValue)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(floatValue) swizzledSelector:@selector(py_floatValue)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(description) swizzledSelector:@selector(py_description)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(componentsSeparatedByString:) swizzledSelector:@selector(py_componentsSeparatedByString:)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(objectForKey:) swizzledSelector:@selector(py_objectForKey:)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(boolValue) swizzledSelector:@selector(py_boolValue)];
+                [objc_getClass("NSNull") py_swizzleMethod:@selector(rangeOfCharacterFromSet:) swizzledSelector:@selector(py_rangeOfCharacterFromSet:)];
             }
         }
     });
 }
 
+
+
 - (NSInteger)py_replace_length {
     return 0;
+}
+
+- (NSInteger)py_integerValue { return 0; };
+
+- (float)py_floatValue { return 0; };
+
+- (NSString *)py_description { return @"0(NSNull)"; }
+
+- (NSArray *)py_componentsSeparatedByString:(NSString *)separator { return @[]; }
+
+- (id)py_objectForKey:(id)key { return nil; }
+
+- (BOOL)py_boolValue { return NO; }
+
+- (NSRange)py_rangeOfCharacterFromSet:(NSCharacterSet *)aSet{
+    NSRange nullRange = {NSNotFound, 0};
+    return nullRange;
 }
 
 @end
